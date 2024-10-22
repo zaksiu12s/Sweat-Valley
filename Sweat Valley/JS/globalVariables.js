02f6050ab4ffe23cd297624b0f40883ff2409ec9
zaksiu12s <zaksiu279@gmail.com>
2024-10-22 20:31:17 +0200

    Git initialization

--- Sweat Valley/JS/globalVariables.js
+++
@@ -1,9 +0,0 @@
-//global variables
-window.mapChange = true
-window.currentMap = islands
-window.currentMapChange = islands
-
-window.center = {
-    x: (canvas.width - 64) / 2,
-    y: (canvas.height - 64) / 2
-}
